5f167f21314af2dd9975f191d5e649b47e829f80
akylalryahalshykhy-boop <akylalryahalshykhy@gmail.com>
2026-06-11 17:58:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # إعدادات سناد يافا
-API_ID = 26569145  # تأكد من هذا الرقم من my.telegram.org
-API_HASH = "775e533e43344654b0366f07a7806132"
+API_ID = 6  # تأكد من هذا الرقم من my.telegram.org
+API_HASH = "eb06d4ab64858f96409890141f237976"
 BOT_TOKEN = "8991880142:AAHYIVF2j037TGYlqccQoJfUpPsaiGEaC1w"
